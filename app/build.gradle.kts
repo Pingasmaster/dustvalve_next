31c4e1d7848df0dfc56b4a159ae16795aa3713a2
Pingasmaster <93002614+Pingasmaster@users.noreply.github.com>
2026-03-21 12:04:40 +0100

    readme changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.dustvalve.next.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 98
-        versionName = "0.0.98"
+        versionCode = 99
+        versionName = "0.0.99"
     }
 
     signingConfigs {
