a9ddb6ba4df8e1ee32ab1725265a70cb9484e65b
Pingasmaster <93002614+Pingasmaster@users.noreply.github.com>
2026-05-06 23:49:46 +0200

    v0.4.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.dustvalve.next.android"
         minSdk = 26
         targetSdk = 37
-        versionCode = 217
-        versionName = "0.4.10"
+        versionCode = 218
+        versionName = "0.4.11"
         versionNameSuffix = "-legacy"
     }
 
